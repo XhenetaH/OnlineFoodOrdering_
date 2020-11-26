717b41f8110cb70074364cfa0b6bc0be80837dbb
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-11-26 19:24:24 +0100

    SubCategoryController and Index method created

---
+++ OnlineFoodOrdering/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineFoodOrdering.Data;
+
+namespace OnlineFoodOrdering.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        //GET - Index
+        public async  Task<IActionResult> Index()
+        {
+            return View(await _db.SubCategory.ToListAsync());
+        }
+    }
+}
