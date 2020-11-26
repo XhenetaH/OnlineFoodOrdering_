bac66944c01299c0d3d4b0b51dad4c4f5b5a61e6
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-11-26 16:34:45 +0100

    Admin and Customer Areas Created

--- OnlineFoodOrdering/Areas/Customer/Controllers/HomeController.cs
+++ OnlineFoodOrdering/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using OnlineFoodOrdering.Models;
 
 namespace OnlineFoodOrdering.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
