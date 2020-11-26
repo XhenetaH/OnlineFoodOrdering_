975cf12b7a626ed0fb1587edeb5620602092d8c9
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-11-26 20:13:57 +0100

    Get Create Method for SubCategory and Extensions created

--- OnlineFoodOrdering/Areas/Admin/Controllers/SubCategoryController.cs
+++ OnlineFoodOrdering/Areas/Admin/Controllers/SubCategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineFoodOrdering.Data;
+using OnlineFoodOrdering.Models.ViewModels;
 
 namespace OnlineFoodOrdering.Areas.Admin.Controllers
 {
@@ -21,6 +22,18 @@ namespace OnlineFoodOrdering.Areas.Admin.Controllers
         {
             var subCategories = await _db.SubCategory.Include(s => s.Category).ToListAsync();
             return View(subCategories);
+        }
+
+        //GET - Create
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+            return View(model);
         }
     }
 }
---
+++ OnlineFoodOrdering/Extensions/ReflectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineFoodOrdering.Extensions
+{
+    public static class ReflectionExtension
+    {
+        public static string GetPropertyValue<T>(this T item, string propertryName)
+        {
+            return item.GetType().GetProperty(propertryName).GetValue(item, null).ToString();
+        }
+    }
+}
