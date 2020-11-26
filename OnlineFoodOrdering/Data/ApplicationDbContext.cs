eb14825890f25aa2c185f590783de64c7bf8822d
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-11-26 17:08:12 +0100

    Model Category Created ang CategoryMigration added to database

--- OnlineFoodOrdering/Data/ApplicationDbContext.cs
+++ OnlineFoodOrdering/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OnlineFoodOrdering.Models;
 
 namespace OnlineFoodOrdering.Data
 {
@@ -12,5 +13,7 @@ namespace OnlineFoodOrdering.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Category { get; set; }
     }
 }
