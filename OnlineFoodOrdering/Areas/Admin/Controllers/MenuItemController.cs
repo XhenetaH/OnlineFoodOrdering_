c98a711507291d181ab82f01445c86fe572c0389
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-11-27 01:42:31 +0100

    MenuItem View Model and Menu Item Get Create Method created

--- OnlineFoodOrdering/Areas/Admin/Controllers/MenuItemController.cs
+++ OnlineFoodOrdering/Areas/Admin/Controllers/MenuItemController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineFoodOrdering.Data;
+using OnlineFoodOrdering.Models.ViewModels;
 
 namespace OnlineFoodOrdering.Areas.Admin.Controllers
 {
@@ -14,15 +15,28 @@ namespace OnlineFoodOrdering.Areas.Admin.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly IWebHostEnvironment _hostingEnvironment;
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
         public MenuItemController(ApplicationDbContext db, IWebHostEnvironment hostingEnvironment)
         {
             this._db = db;
             this._hostingEnvironment = hostingEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
         public async Task<IActionResult> Index()
         {
             var menuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+
+        //GET - Create
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
