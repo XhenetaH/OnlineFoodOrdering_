79fad1ccc8d299dc59996f04c1c6c07b98439875
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-11-27 01:26:26 +0100

    MenuItemController and Index Method Crated

---
+++ OnlineFoodOrdering/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineFoodOrdering.Data;
+
+namespace OnlineFoodOrdering.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment hostingEnvironment)
+        {
+            this._db = db;
+            this._hostingEnvironment = hostingEnvironment;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
+            return View(menuItems);
+        }
+    }
+}
