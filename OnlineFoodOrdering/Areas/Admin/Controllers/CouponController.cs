3332d8fd83e529ae47e24251696e78822700881b
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-12-02 16:35:33 +0100

    Create Post Method added

--- OnlineFoodOrdering/Areas/Admin/Controllers/CouponController.cs
+++ OnlineFoodOrdering/Areas/Admin/Controllers/CouponController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineFoodOrdering.Data;
+using OnlineFoodOrdering.Models;
 using OnlineFoodOrdering.Utility;
 
 namespace OnlineFoodOrdering.Areas.Admin.Controllers
@@ -27,6 +29,33 @@ namespace OnlineFoodOrdering.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupons)
+        {
+            if(ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if(files.Count>0)
+                {
+                    byte[] p1 = null;
+                    using(var fs1 = files[0].OpenReadStream())
+                    {
+                        using(var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupons.Picture = p1;
+                }
+                _db.Coupon.Add(coupons);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupons);
         }
     }
 }
