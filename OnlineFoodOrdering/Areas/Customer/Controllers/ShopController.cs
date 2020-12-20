6aff1aa40465b54bfe4b6d52f220a67e47c1076c
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-12-21 00:47:38 +0100

    Search functionality added to Shop

--- OnlineFoodOrdering/Areas/Customer/Controllers/ShopController.cs
+++ OnlineFoodOrdering/Areas/Customer/Controllers/ShopController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineFoodOrdering.Data;
+using OnlineFoodOrdering.Models;
 using OnlineFoodOrdering.Models.ViewModels;
 
 namespace OnlineFoodOrdering.Areas.Customer.Controllers
@@ -17,16 +19,33 @@ namespace OnlineFoodOrdering.Areas.Customer.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    IndexViewModel IndexVM = new IndexViewModel()
+        //    {
+        //        MenuItem = _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToList(),
+        //        Category = _db.Category.ToList()
+
+        //    };
+        //    return View(IndexVM);
+        //}
+
+        public IActionResult Index(string searchString)
         {
+            var menuItem = from m in _db.MenuItem
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                menuItem = menuItem.Include(c=>c.Category).Where(s => s.Name.Contains(searchString));
+            }
+
             IndexViewModel IndexVM = new IndexViewModel()
             {
-                MenuItem = _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToList(),
+                MenuItem = menuItem,
                 Category = _db.Category.ToList()
-
             };
             return View(IndexVM);
         }
-
     }
 }
