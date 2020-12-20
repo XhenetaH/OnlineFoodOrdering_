c58bf9f5f0e18069a2c14de8cb8a688f014d8900
Xheneta Hysenlekaj <xhe_neta@live.com>
2020-12-20 18:27:38 +0100

    Shop Controller and Index method created

--- OnlineFoodOrdering/Areas/Customer/Controllers/HomeController.cs
+++ OnlineFoodOrdering/Areas/Customer/Controllers/HomeController.cs
@@ -131,16 +131,5 @@ namespace OnlineFoodOrdering.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult Search()
-        {
-            IndexViewModel IndexVM = new IndexViewModel()
-            {
-                MenuItem = _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToList(),
-                Category = _db.Category.ToList()
-
-            };            
-            return View(IndexVM);
-        }
-
     }
 }
---
+++ OnlineFoodOrdering/Areas/Customer/Controllers/ShopController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineFoodOrdering.Data;
+using OnlineFoodOrdering.Models.ViewModels;
+
+namespace OnlineFoodOrdering.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ShopController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                MenuItem = _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToList(),
+                Category = _db.Category.ToList()
+
+            };
+            return View(IndexVM);
+        }
+
+    }
+}
